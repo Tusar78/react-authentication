4e3d95f5f4883a14d26e7ac5485656fde6a3af3f
Tusar <tusarahmed141@gmail.com>
2022-08-24 16:53:05 +0600

    Prepare to again authenticate

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -1,27 +1,8 @@
-import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-const auth = getAuth(app);
-
 function App() {
-  const provider = new GoogleAuthProvider();
-  const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        console.log(error);
-      });
-  };
   return (
     <>
       <button
         className="px-6 py-2 bg-blue-600 text-white rounded-md m-12 focus:ring-2 ring-offset-2 ring-blue-300"
-        onClick={handleGoogleSignIn}
       >
         Sign in Google
       </button>

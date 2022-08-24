61d17ad102fa99e2cec00a9f37405d63f2426583
Tusar <tusarahmed141@gmail.com>
2022-08-24 22:23:25 +0600

    Configure firebase

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -1,8 +1,31 @@
+import app from "./firebase.init";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
+
 function App() {
+  const [user, setUser] = useState({})
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const handleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      })
+  };
+
   return (
     <>
       <div className="m-12">
-        <button className="px-6 py-2 bg-blue-600 text-white rounded-md focus:ring-2 ring-offset-2 ring-blue-300">
+        <button
+          className="px-6 py-2 bg-blue-600 text-white rounded-md focus:ring-2 ring-offset-2 ring-blue-300"
+          onClick={handleSignIn}
+        >
           Sign in Google
         </button>
         <button className="px-6 py-2 ml-4 bg-red-600 text-white rounded-md focus:ring-2 ring-offset-2 ring-red-300">
@@ -10,9 +33,9 @@ function App() {
         </button>
 
         <div className="mt-4 w-96 p-4 border rounded-md space-y-4">
-          <img src="" alt="" />
-          <p className="border-b border-blue-300 pb-2">Name: {}</p>
-          <p className="border-b border-blue-300 pb-2">Email: {}</p>
+          <img src={user.photoURL} alt={user.displayName} />
+          <p className="border-b border-blue-300 pb-2">Name: {user.displayName}</p>
+          <p className="border-b border-blue-300 pb-2">Email: {user.email}</p>
         </div>
       </div>
     </>

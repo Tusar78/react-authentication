11c215aaa719d5f5c4cc65b839f01a0b0442cf33
Tusar <tusarahmed141@gmail.com>
2022-08-24 18:26:12 +0600

    Sign in and sign out functionality is done

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -1,28 +1,66 @@
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({});
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const handleGoogle = () => {
+
+  const handleSignIn = () => {
     signInWithPopup(auth, provider)
-      .then(result => {
+      .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
-      .catch(error => {
+      .catch((error) => {
         const errorMessage = error.message;
         console.log(errorMessage);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
       })
-  }
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
   return (
     <>
-      <button
-        className="px-6 py-2 bg-blue-600 text-white rounded-md m-12 focus:ring-2 ring-offset-2 ring-blue-300"
-        onClick={handleGoogle}
-      >
-        Sign in Google
-      </button>
+      <div className="m-12">
+        {!user.email ? (
+          <button
+            className="px-6 py-2 bg-blue-600 text-white rounded-md focus:ring-2 ring-offset-2 ring-blue-300"
+            onClick={handleSignIn}
+          >
+            Sign in Google
+          </button>
+        ) : (
+          <button
+            className="px-6 py-2 ml-4 bg-red-600 text-white rounded-md focus:ring-2 ring-offset-2 ring-red-300"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
+        )}
+        <div className="mt-4 w-96 p-4 border rounded-md space-y-4">
+          <img src={user?.photoURL} alt={user.displayName} />
+          <p className="border-b border-blue-300 pb-2">
+            Name: {user.displayName}
+          </p>
+          <p className="border-b border-blue-300 pb-2">Email: {user.email}</p>
+        </div>
+      </div>
     </>
   );
 }

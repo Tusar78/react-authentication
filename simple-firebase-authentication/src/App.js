b34f17916287a194b5a44c4434bc8db83846df2f
Tusar <tusarahmed141@gmail.com>
2022-08-24 16:52:24 +0600

    Google authetication done

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -1,12 +1,30 @@
 import app from "./firebase.init";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        console.log(error);
+      });
+  };
   return (
     <>
-      <h2 className="text-red-600">Hello React!</h2>
+      <button
+        className="px-6 py-2 bg-blue-600 text-white rounded-md m-12 focus:ring-2 ring-offset-2 ring-blue-300"
+        onClick={handleGoogleSignIn}
+      >
+        Sign in Google
+      </button>
     </>
   );
 }
--- simple-firebase-authentication/src/_steps.js
+++ simple-firebase-authentication/src/_steps.js
@@ -6,5 +6,8 @@
  * 4. Copy firebase init with config from firebase project settings into a file firebase.init.js
  * 5. Export default app
  * 6. Import app firebase init.js into your app.js
- * 7. Import getAuth from firebase/auth and create auth = getAuth(app)
+ * 7. Import getAuth from firebase/auth and create auth = getAuth(app).
+ * 8. Turn on google authentication (firebase > authentication > enable google sign in).
+ * 9. Create google provider.
+ * 10. 
  */

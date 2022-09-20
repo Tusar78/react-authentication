438b05cbc54face9ec25a2d9722b6ea8a09d4203
Tusar <tusarahmed141@gmail.com>
2022-09-20 11:46:27 +0600

    navbar design complete

--- router-firebase-integration/src/Components/Navbar/Navbar.js
+++ router-firebase-integration/src/Components/Navbar/Navbar.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <nav className="nav custom-grid">
-        <h1 className='nav__logo'>RFI</h1>
+        <h1 className="nav__logo">RFI</h1>
 
-        <div className="nav__menu">
+        <div className={toggle ? "nav__menu right-0" : "nav__menu"}>
           <ul className="nav__list">
-            <li className="nav__item"><a href="/" className="nav__link">Home</a></li>
-            <li className="nav__item"><a href="/" className="nav__link">Product</a></li>
-            <li className="nav__item"><a href="/" className="nav__link">Order</a></li>
-            <li className="nav__item"><a href="/" className="nav__link">Register</a></li>
-            <li className="nav__item"><a href="/" className="nav__link">Login</a></li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                Product
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                Order
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                Register
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="/" className="nav__link">
+                Login
+              </a>
+            </li>
           </ul>
         </div>
 
-        <button className='nav__toggle'>
-          
+        <button className="nav__toggle" onClick={() => setToggle(!toggle)}>
+          {toggle ? (
+            <MdClose className="nav__toggle-icon" />
+          ) : (
+            <HiMenuAlt4 className="nav__toggle-icon" />
+          )}
         </button>
       </nav>
     </div>

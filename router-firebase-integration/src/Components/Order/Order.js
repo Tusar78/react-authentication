b58a4b03247b86e97c238d972a0d09ff6bb45488
Tusar <tusarahmed141@gmail.com>
2022-09-21 18:16:04 +0600

    Required auth system is ready i mean protected route is ready

--- router-firebase-integration/src/Components/Order/Order.js
+++ router-firebase-integration/src/Components/Order/Order.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Order = () => {
   return (
-    <div>
-      <h2>Order section</h2>
-    </div>
+    <section className='section'>
+      <div className="order custom-grid">
+        <h2>Order Section</h2>
+
+        <div className="order-container">
+          <ul className="order__list">
+            <li className="order__item">Hp Monitor</li>
+            <li className="order__item">Intel Core i5 6485</li>
+            <li className="order__item">Micromax Sound System</li>
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 };
 

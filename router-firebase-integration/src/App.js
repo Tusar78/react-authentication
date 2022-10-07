2f229bbb644013256b1b8b68658f0fd9332de52b
Tusar <tusarahmed141@gmail.com>
2022-10-07 15:04:09 +0600

    change name order to user

--- router-firebase-integration/src/App.js
+++ router-firebase-integration/src/App.js
@@ -2,9 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
-import Order from "./Components/Order/Order";
 import Product from "./Components/Product/Product";
 import Register from "./Components/Register/Register";
+import Users from "./Components/Users/Users";
 // import RequireAuth from "./Components/RequireAuth/RequireAuth";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/order" element={<Order />} />
+        <Route path="/order" element={<Users />} />
         <Route path="/product" element={<Product />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
---
+++ router-firebase-integration/src/Components/Users/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <section className='section'>
+      <div className="users custom-grid">
+        <h2>Users Section</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Users;

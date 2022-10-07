e2a36f75eb68e54c1f29997acca1481b27378f1f
Tusar <tusarahmed141@gmail.com>
2022-10-07 17:20:07 +0600

    User section done

--- router-firebase-integration/src/Components/Users/Users.js
+++ router-firebase-integration/src/Components/Users/Users.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SingleUser from '../SingleUsers/SingleUser';
+import useUsers from '../useUsers/useUsers';
 
 const Users = () => {
+  const {users} = useUsers('https://jsonplaceholder.typicode.com/users');
   return (
     <section className='section'>
-      <div className="users custom-grid">
-        <h2>Users Section</h2>
+      <div className="users-container custom-grid mt-8">
+        {
+          users?.map(user => <SingleUser key={user.id} user={user} />)
+        }
       </div>
     </section>
   );

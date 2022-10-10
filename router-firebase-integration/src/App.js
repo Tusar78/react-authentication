6a766e2c719c8be69ea9bf671194feadc684bcdc
Tusar <tusarahmed141@gmail.com>
2022-10-10 06:34:10 +0600

    |Last title update

--- router-firebase-integration/src/App.js
+++ router-firebase-integration/src/App.js
@@ -1,13 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import PageNotFound from "./Components/PageNotFound/PageNotFound";
+import Posts from "./Components/Posts/Posts";
 import Footer from "./Components/ShareComponent/Card/Footer/Footer";
+import SignIn from "./Components/SignIn/SignIn";
+import Users from "./Components/Users/Users";
 
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users" element={<Posts />} />
+        <Route path="/users" element={<SignIn />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
       <Footer />
     </>
   );
---
+++ router-firebase-integration/src/Components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Posts = () => {
+  return (
+    <div>
+      this is post section
+    </div>
+  );
+};
+
+export default Posts;

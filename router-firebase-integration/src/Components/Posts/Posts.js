3c5646225744a6fa22cbf520472465cd657bad1b
Tusar <tusarahmed141@gmail.com>
2022-10-10 12:45:40 +0600

    Load post data

--- router-firebase-integration/src/Components/Posts/Posts.js
+++ router-firebase-integration/src/Components/Posts/Posts.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import useUsers from "../useUsers/useUsers";
 
 const Posts = () => {
+  const { users } = useUsers('https://jsonplaceholder.typicode.com/posts');
+  
   return (
     <section className='post-section'>
-      <div className="post__title title"></div>
-      this is post section
+      <h2 className="post__title title">Our Blog Post</h2>
+      
+      <div className="post__container custom-grid">
+        {users?.length}
+      </div>
     </section>
   );
 };
